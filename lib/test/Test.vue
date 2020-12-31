808d08611fe031c94fb91450ef189c768e17c365
Edenlw <568885816@qq.com>
2020-12-31 16:43:09 +0800

    init

---
+++ lib/test/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    {{`fjalfsdf${num}-${msg}`}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  props: {
+    num: {
+      type: Number,
+      default: 0
+    },
+    msg: {
+      type: String,
+      default: "test"
+    }
+  },
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  mounted(){},
+  methods: {
+
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
