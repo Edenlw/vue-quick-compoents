922a1357451d064fdc6a30d66b1405b3c951eb36
Edenlw <568885816@qq.com>
2021-01-06 10:52:00 +0800

    测试husky

--- examples/App.vue
+++ examples/App.vue
@@ -1,13 +1,41 @@
+<!--
+ * @Author: liuwei
+ * @Date: 2020-12-30 13:15:40
+ * @LastEditors: liuwei
+ * @LastEditTime: 2021-01-06 10:46:10
+ * @Description: file content
+-->
 <template>
   <div id="app">
     <test />
+    <div v-for="item in list" :key="item"></div>
+    <img src="" alt="" />
   </div>
 </template>
-
 <script>
-import {Test} from "../lib/index";
+import { Test } from '../lib/index';
 export default {
+  props: {
+    list: Array,
+    default: () => {}
+  },
   name: "App",
+  methods: {
+    aaa() {
+
+
+      
+      let a = 3;
+      let b = 5;
+
+
+
+      if (a === b) {
+        console.log(b);
+      }
+      console.log(a, b, 123);
+    }
+  },
   components: {
     Test
   }
